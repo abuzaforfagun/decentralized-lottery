c64d49efbf8a35b9e3fad8543b9f8cf260f92935
Fagun <mdabuzaforfagun@gmail.com>
2025-01-19 13:15:49 +0600

    Use VRF to generate random numbers

--- smart-contract/lottery/script/DeployLottery.s.sol
+++ smart-contract/lottery/script/DeployLottery.s.sol
@@ -4,18 +4,61 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {Lottery} from "../src/Lottery.sol";
+import {VRFCoordinatorV2_5Mock} from "../mock/chainlink/VRFCoordinatorV2_5Mock.sol";
 
 contract DeployLottery is Script {
     uint256 private constant ENTRY_FEE = 0.001 ether;
-    uint256 private constant NUMBER_OF_PARTICIPANTS_REQUIRE_TO_DRAW = 10;
+    uint256 private constant NUMBER_OF_PARTICIPANTS_REQUIRE_TO_DRAW = 3;
+    address private constant VRF_COORDINATOR =
+        0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B;
+    bytes32 private constant KEY_HASH =
+        0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae;
+    uint256 private constant SUB_ID =
+        6504699982786204825045679599031601495393816841262864436601874575408228222640;
 
     Lottery public lottery;
 
-    function run() public {
+    function run() public returns (Lottery) {
+        uint96 baseFee = 1000000000000000;
+        uint96 gasPriceLink = 50000000000;
+        int256 weiPerunitLink = 10000000000000000;
+
         vm.startBroadcast();
 
-        lottery = new Lottery(ENTRY_FEE, NUMBER_OF_PARTICIPANTS_REQUIRE_TO_DRAW);
+        if (block.chainid == 31337) {
+            VRFCoordinatorV2_5Mock vrfCoordinatorV2_5Mock = new VRFCoordinatorV2_5Mock(
+                    baseFee,
+                    gasPriceLink,
+                    weiPerunitLink
+                );
+            uint256 subscriptionId = vrfCoordinatorV2_5Mock
+                .createSubscription();
+            vrfCoordinatorV2_5Mock.fundSubscription(subscriptionId, 10 ether);
+
+            lottery = new Lottery(
+                ENTRY_FEE,
+                NUMBER_OF_PARTICIPANTS_REQUIRE_TO_DRAW,
+                address(vrfCoordinatorV2_5Mock),
+                KEY_HASH,
+                subscriptionId
+            );
+
+            vrfCoordinatorV2_5Mock.addConsumer(
+                subscriptionId,
+                address(lottery)
+            );
+        } else {
+            lottery = new Lottery(
+                ENTRY_FEE,
+                NUMBER_OF_PARTICIPANTS_REQUIRE_TO_DRAW,
+                VRF_COORDINATOR,
+                KEY_HASH,
+                SUB_ID
+            );
+        }
 
         vm.stopBroadcast();
+
+        return lottery;
     }
 }
--- smart-contract/lottery/src/Lottery.sol
+++ smart-contract/lottery/src/Lottery.sol
@@ -2,7 +2,10 @@
 
 pragma solidity ^0.8.19;
 
-contract Lottery {
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
+contract Lottery is VRFConsumerBaseV2Plus {
     error Lottery_InsufficiantFund();
     error Lottery_NotOpened();
     error Lottery_InvalidState();
@@ -20,18 +23,32 @@ contract Lottery {
     address private s_owner;
     uint256 private s_lastRoundStartedAt;
     Status private s_lotteryStatus;
+    uint256 private s_requestID;
 
     uint256 private immutable i_entryFee;
     uint256 private immutable i_numberOfParticipantsRequiredToDraw;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subId;
 
-    uint256 private constant PLATFORM_COMMISION_IN_PERCENTAGE = 5;
+    uint256 private constant PLATFORM_COMMISION_IN_PERCENTAGE = 10;
+    uint32 private constant NUM_OF_WORDS = 1;
+    uint16 private constant NUM_OF_REQUEST_CONFIRMATION = 3;
+    uint32 private constant CALLBACK_GAS_LIMIT = 2500000;
 
-    constructor(uint256 entryFee, uint256 numberOfParticipantsRequiredToDraw) {
+    constructor(
+        uint256 entryFee,
+        uint256 numberOfParticipantsRequiredToDraw,
+        address vrfCoordinator,
+        bytes32 keyHash,
+        uint256 subId
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entryFee = entryFee;
         s_lastRoundStartedAt = block.timestamp;
         s_owner = msg.sender;
         s_lotteryStatus = Status.ONGOING;
         i_numberOfParticipantsRequiredToDraw = numberOfParticipantsRequiredToDraw;
+        i_keyHash = keyHash;
+        i_subId = subId;
     }
 
     function status() external view returns (Status) {
@@ -50,7 +67,37 @@ contract Lottery {
         return s_lastRoundWinner;
     }
 
-    function join() external payable {
+    function getVrfRequestId() external view returns (uint256) {
+        return s_requestID;
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal override {
+        uint256 totalParticipants = s_participantes.length;
+
+        uint256 winnerIndex = randomWords[0] % totalParticipants;
+
+        s_lastRoundWinner = s_participantes[winnerIndex];
+
+        uint256 platformComision = (address(this).balance *
+            PLATFORM_COMMISION_IN_PERCENTAGE) / 100;
+        uint256 winningPrize = address(this).balance - platformComision;
+
+        (bool success, ) = s_lastRoundWinner.call{value: winningPrize}("");
+
+        if (!success) {
+            revert Lottery_PaymentFailed();
+        }
+        s_participantes = new address[](0);
+        s_lotteryStatus = Status.ONGOING;
+
+        s_owner.call{value: address(this).balance}("");
+        s_requestID = 0;
+    }
+
+    function join() external payable returns (uint256) {
         if (msg.value < i_entryFee) {
             revert Lottery_InsufficiantFund();
         }
@@ -62,11 +109,12 @@ contract Lottery {
         s_participantes.push(msg.sender);
 
         if (s_participantes.length == i_numberOfParticipantsRequiredToDraw) {
-            declareWinner();
+            return declareWinner();
         }
+        return 0;
     }
 
-    function declareWinner() public {
+    function declareWinner() public returns (uint256) {
         if (s_lotteryStatus != Status.ONGOING) {
             revert Lottery_InvalidState();
         }
@@ -77,22 +125,18 @@ contract Lottery {
 
         s_lotteryStatus = Status.CALCULATING;
 
-        //TODO: Use VRF of chainlink
-        uint256 winnerIndex = s_participantes.length / 2;
-
-        s_lastRoundWinner = s_participantes[winnerIndex];
-
-        uint256 platformComision = (address(this).balance * PLATFORM_COMMISION_IN_PERCENTAGE) / 100;
-        uint256 winningPrize = address(this).balance - platformComision;
-
-        (bool success,) = s_lastRoundWinner.call{value: winningPrize}("");
-
-        if (!success) {
-            revert Lottery_PaymentFailed();
-        }
-        s_participantes = new address[](0);
-        s_lotteryStatus = Status.ONGOING;
-
-        s_owner.call{value: address(this).balance}("");
+        s_requestID = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subId,
+                requestConfirmations: NUM_OF_REQUEST_CONFIRMATION,
+                callbackGasLimit: CALLBACK_GAS_LIMIT,
+                numWords: NUM_OF_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+        return s_requestID;
     }
 }
